260cacdc36e0e1b5116553977c2215b021e3d478
[Evan McNeely] <evanmcneely@me.com>
2022-03-30 21:29:31 -0400

    Setup basic webpack and homepage and app pages to start develepment. Also brought ni code for authentication (sessions and user verification) but still needs to be completed with proper database queries

---
+++ server/routes/api.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const userController = require('../controllers/userController');
+const sessionController = require('../controllers/sessionController');
+
+const router = express.Router();
+
+// post -> login
+router.post(
+  '/login',
+  // ! userController.verifyUser,
+  sessionController.startSession,
+  (req, res) => {
+    res.status(200).send();
+  }
+);
+
+// post -> signup
+router.post(
+  '/signup',
+  // ! userController.createUser,
+  sessionController.startSession,
+  (req, res) => {
+    res.status(200).send();
+  }
+);
+
+// post -> logout
+router.post(
+  '/logout',
+  sessionController.checkSession,
+  sessionController.endSession,
+  (req, res) => {
+    res.status(200).send();
+  }
+);
+
+module.exports = router;
+
+
+
+// "@babel/core": "^7.17.7",
+//     "@babel/preset-env": "^7.16.11",
+//     "@babel/preset-react": "^7.16.7",
+//     "babel-loader": "^8.2.3",
+//     " ": "^6.7.1",
+//     "html-webpack-plugin": "^5.5.0",
+//     "node-polyfill-webpack-plugin": "^1.1.4",
+//     " ": "^2.0.15",
+//     "stream-http": "^3.2.0",
+//     "style-loader": "^3.3.1",
+//     "ts-loader": "^9.2.8",
+//     "webpack": "^5.70.0",
+//     "webpack-cli": "^4.9.2",
+//     "webpack-dev-server": "^4.7.4"
---
+++ webpack.config.js
@@ -0,0 +1,66 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: process.env.NODE_ENV,
+  entry: {
+    index: './client/src/index.js',
+    app: './client/src/app.js',
+  },
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: '[name].bundle.js',
+    // publicPath: '/build'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /.(css|scss)$/,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './client/public/index.html',
+      filename: 'index.html',
+      publicPath: process.env.NODE_ENV === 'production' ? 'build' : 'auto',
+      chunks: ['index'],
+    }),
+    new HtmlWebpackPlugin({
+      template: './client/public/app.html',
+      filename: 'app.html',
+      publicPath: process.env.NODE_ENV === 'production' ? 'build' : 'auto',
+      chunks: ['app'],
+    }),
+  ],
+  devServer: {
+    // historyApiFallback: true,
+    hot: true,
+    magicHtml: true,
+    proxy: {
+      '/api': 'http://localhost:3000',
+      '/app': {
+        // ? understand hov this works
+        target: 'http://localhost:3000',
+        bypass: function (req, res, proxyOptions) {
+          if (req.headers.accept.indexOf('html') !== -1) {
+            console.log('Skipping proxy for browser request.');
+            return '/app.html';
+          }
+        },
+      },
+    },
+  },
+};
