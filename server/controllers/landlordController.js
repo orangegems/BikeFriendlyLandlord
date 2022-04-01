4101b943447cbe61e559a6bc1b9f0b901560588c
Michael Snyder <msnyder1992@gmail.com>
2022-03-31 21:30:45 -0400

    Added route to fetch landlords by city

--- server/controllers/landlordController.js
+++ server/controllers/landlordController.js
@@ -18,4 +18,27 @@ landlordController.getAllLandlords = async (req, res, next) => {
   }
 };
 
+landlordController.getLandlordsByCity = async (req, res, next) => {
+  const queryString = `
+        SELECT landlords.*, addresses.city, addresses.state 
+        FROM landlords 
+        INNER JOIN addresses ON landlords._id = addresses.landlord_id
+        WHERE addresses.city = $1;
+    `;
+
+  const { city } = req.body;
+  try {
+    const results = await db.query(queryString, [city]);
+    res.locals.landlords = results.rows;
+    return next();
+  } catch (error) {
+    return next({
+      message:
+        "An error occured attempting to fetch landlords by city in landlordController.getLandlordsByCity",
+      log: "Error: " + error,
+      status: 500,
+    });
+  }
+};
+
 module.exports = landlordController;
--- server/routes/landlord.js
+++ server/routes/landlord.js
@@ -4,7 +4,11 @@ const router = express.Router();
 
 const landlordController = require('../controllers/landlordController.js');
 
-router.get('/', landlordController.getAllLandlords,(req, res) => {
+router.get('/all', landlordController.getAllLandlords,(req, res) => {
+    return res.json(res.locals.landlords);
+});
+
+router.get('/byCity', landlordController.getLandlordsByCity, (req, res) => {
     return res.json(res.locals.landlords);
 });
 
