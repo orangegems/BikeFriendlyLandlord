215f0789fb2b06168da7019c0325aac61f253a13
Jonathan Luu <jonathanluu17@gmail.com>
2022-03-31 19:49:18 -0700

    Completed all front end components, need to add submit and cancel functionality

--- client/src/components/ReviewPage.jsx
+++ client/src/components/ReviewPage.jsx
@@ -12,7 +12,7 @@ import Container from '@mui/material/Container';
 
 
 export default function ReviewPage() {
-    // handle title input
+    // handle title input (limit 100)
     const [title, setTitle] = React.useState('');
     const handleTitleChange = (e) => {
         setTitle(e.target.value);
@@ -24,13 +24,13 @@ export default function ReviewPage() {
     const [bike, setBike] = React.useState(null)
     const [pet, setPet] = React.useState(null)
 
-    //handle description input
+    //handle description input (limit 1000)
     const [description, setDescription] = React.useState('');
     const handleDescChange = (e) => {
         setDescription(e.target.value);
     }
 
-    // method to handle going back
+    // method to handle form submission
 
 
         // will need the Landlord name from somewhere (props?)
@@ -44,7 +44,8 @@ export default function ReviewPage() {
                         className="reviewformContainer"
                         sx={
                             {p: 2, border: '1px solid grey'}
-                        }>
+                        }
+                        >
                             <h2>Review of {'landlordnamehere'}</h2>
                             <TextField 
                                 fullWidth 
@@ -52,23 +53,26 @@ export default function ReviewPage() {
                                 label="Title"
                                 value={title}
                                 onChange={handleTitleChange}
+                                inputProps={{maxLength:100}}
+                                helperText="Max 100 Characters"
                                 />
-                            <Stack direction="row" spacing={2}>
+                            <Stack direction="row" spacing={2} justifyContent="center">
                                 <h2>Overall Rating</h2>
                                 <Rating required size="large" precision={0.5} value={ 5 } readOnly />
                             </Stack>
-                            <Stack direction="row" spacing={2}>
+                            <Stack direction="row" spacing={2} justifyContent="center">
                                 <h2>Respectiveness</h2>
                                 <Rating required size="large" precision={0.5} value={ respect } onChange={(e, val) => setRespect(val)}/>
                             </Stack>
-                            <Stack direction="row" spacing={2}>
+                            <Stack direction="row" spacing={2} justifyContent="center">
                                 <h2>Responsiveness</h2>
                                 <Rating required size="large" precision={0.5} value={ response } onChange={(e, val) => setResponse(val)}/>
                             </Stack>
-                            <Stack direction="row" spacing={2}>
+                            <Stack direction="row" spacing={2} justifyContent="center">
                                 <h2>Bike Friendly</h2>
                                 <Rating required size="large" precision={0.5} value={ bike } onChange={(e, val) => setBike(val)}/>
-                            </Stack>                            <Stack direction="row" spacing={2}>
+                            </Stack>                            
+                            <Stack direction="row" spacing={2} justifyContent="center">
                                 <h2>Pet Friendly</h2>
                                 <Rating required size="large" precision={0.5} value={ pet } onChange={(e, val) => setPet(val)}/>
                             </Stack>
@@ -78,12 +82,25 @@ export default function ReviewPage() {
                                 label="Additional Comments"
                                 multiline
                                 rows={4}
-                                inputProps={{maxLength:200}}
-                                helperText="Max 200 Characters"
+                                inputProps={{maxLength:1000}}
+                                helperText="Max 1000 Characters"
                                 value={ description }
                                 onChange={ handleDescChange}
                             />
-                        </Box>
+                            <Stack direction="row" spacing={2} justifyContent="flex-end">
+                                <Button
+                                    variant="outlined"
+                                >
+                                    Cancel
+                                </Button>
+                                <Button
+                                    variant="contained"
+                                >
+                                    Submit
+                                </Button>
+                            </Stack>
+                    </Box>
+
             </Container>
         )
 }
