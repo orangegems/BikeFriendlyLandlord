c85804df375474f85ce470ff6377a5bd00316af9
[Evan McNeely] <evanmcneely@me.com>
2022-04-02 16:54:05 -0400

    Added route and middleware to get all reviews left by a user. User profile is getting these reviews. Not tested

--- client/src/pages/user/UserProfile.jsx
+++ client/src/pages/user/UserProfile.jsx
@@ -2,27 +2,51 @@ import React, { useState, useEffect } from 'react';
 import { useSearchParams, useNavigate } from 'react-router-dom';
 
 export function UserProfile(props) {
-  const {userData, setUserData, setIsLoggedIn, setAuthDisplay} = props;
+  const { userData, setUserData, setIsLoggedIn, setAuthDisplay } = props;
+  const [reviews, setReviews] = props;
   let navigate = useNavigate();
-  
-  useEffect(()=> {
+
+  useEffect(() => {
     fetch('/auth/check', {
       method: 'POST',
-    }) 
-    .then((res)=> {
-      if (res.status !== 200) {
-        setIsLoggedIn(false);
-        setAuthDisplay(true);
-        navigate("/")
-      }
     })
-  },[])
-  
+      .then((res) => {
+        if (res.status === 401) {
+          setIsLoggedIn(false);
+          setAuthDisplay(true);
+          navigate('/');
+        }
+      })
+      .catch((err) => {
+        console.log('Error check login -->', err);
+      });
+
+    fetch(`/reviews/${userData._id}`)
+      .then((res) => {
+        if (res.status === 401) {
+          setIsLoggedIn(false);
+          setAuthDisplay(true);
+          navigate('/');
+        } else {
+          return res.json();
+        }
+      })
+      .then((json) => {
+        console.log('check response -->', json);
+        setReviews(json.reviews);
+      })
+      .catch((err) => {
+        console.log('Error fenching users reviews -->', err);
+      });
+  }, []);
+
   // let [searchParams, setSearchParams] = useSearchParams();
   return (
-  <div>
-    <p>Hello {' '} {userData.full_name}{','}</p>
-
-  </div>
-  )
+    <div>
+      <p>
+        Hello {userData.full_name}
+        {','}
+      </p>
+    </div>
+  );
 }
