fe5cbc21470b5a3483ea772b893061134aafc572
Michael Snyder <msnyder1992@gmail.com>
2022-04-04 17:00:38 -0400

    Included the ability to dynamically render landlord images on homepage

--- client/src/components/homeCard/HomeCard.jsx
+++ client/src/components/homeCard/HomeCard.jsx
@@ -19,7 +19,7 @@ export default function HomeCard({ landlord }) {
         component="img"
         alt="landlord pic"
         height="200"
-        image="https://images.unsplash.com/photo-1499887142886-791eca5918cd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"
+        image={`http://localhost:3000/images/${landlord.profile_pic}`}
       />
       <CardContent>
         <div className="homeCardContents">
