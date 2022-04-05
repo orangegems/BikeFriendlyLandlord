08204f7bc67131972024746296ba0e1c6f497c05
nukiyaka <93238813+nukiyaka@users.noreply.github.com>
2022-04-04 19:31:25 -0700

    no rendering issue

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import { Authenticate } from "./pages/authenticate/Authenticate.jsx";
 import { Navbar } from "./components/navbar/Navbar.jsx";
 import ClearNavbar from "./components/clearNavbar/ClearNavbar.jsx";
 import Home from "./pages/home/Home.jsx";
-import { Profile } from './pages/profile/Profile.jsx';
+import Profile from './pages/profile/Profile.jsx';
 import  Search  from './pages/search/Search.jsx';
 import { UserProfile } from './pages/user/UserProfile.jsx';
 import { Route, Routes, Link } from 'react-router-dom';
