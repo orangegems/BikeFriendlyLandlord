80b63865427b1921f96ddce9e6debd8ed8615dcc
Jonathan Luu <jonathanluu17@gmail.com>
2022-04-05 18:01:31 -0700

    troubleshooting search

---
+++ client/src/components/searchResults/resultDisplay.jsx
@@ -0,0 +1,17 @@
+import React, { useEffect} from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+
+export default function resultDisplay() {
+    // grab the constants from our params (useparams?)
+
+    // set up onclick to route to the respective landlord id
+
+    // render table with our fetched results
+    
+}
