8cefea244ee123f1cc7bb4f3116534d4f8a8068d
Jon Dewey <donjewey@gmail.com>
2022-04-13 00:16:33 -0700

    added redux skeleton

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,36 +1,44 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
+import { Route, Routes, Link } from "react-router-dom";
+import { connect } from "react-redux";
 
-import { Navbar } from './components/navbar/Navbar.jsx';
-import Home from './pages/home/Home.jsx';
-import MapSearch from './pages/map/MapSearch.jsx';
-import Profile from './pages/profile/Profile.jsx';
-import Search from './pages/search/Search.jsx';
-import { ReviewPage } from './pages/review/ReviewPage.jsx';
-import { UserProfile } from './pages/user/UserProfile.jsx';
-import { Route, Routes, Link } from 'react-router-dom';
+import * as actions from "./actions/actions.js";
+
+// components
+import { Navbar } from "./components/navbar/Navbar.jsx";
+import Home from "./pages/home/Home.jsx";
+import MapSearch from "./pages/map/MapSearch.jsx";
+import Profile from "./pages/profile/Profile.jsx";
+import Search from "./pages/search/Search.jsx";
 import Footer from "./components/footer/Footer.jsx";
+import { ReviewPage } from "./pages/review/ReviewPage.jsx";
+import { UserProfile } from "./pages/user/UserProfile.jsx";
 
-export function App() {
+// called with this.props.[currentUser],
+// references global redux state
+const mapStateToProps = (state) => ({
+  userData: state.currentUser.data,
+});
+
+// called with this.props.[setUserData],
+// dispatches action upon invokation
+const mapDispatchToProps = (dispatch) => ({
+  setUserData: (userData) => dispatch(actions.setUserData(userData)),
+  resetUserData: () => dispatch(actions.resetUserData()),
+});
+
+const App = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [authDisplay, setAuthDisplay] = useState(false);
 
   const [userData, setUserData] = useState({});
 
   useEffect(() => {
-    fetch('/user/getUser')
-      // .then((res) => {
-      //   if (res.status === 200) {
-      //     return res.json();
-      //   } else {
-      //     // break the promise chain from updating state
-      //     return { then: function () {} };
-      //   }
-      // })
+    fetch("/user/getUser")
       .then((res) => {
-        // parsing the response will error if the user is not authenticated and no data got returned
-        return res.json()
+        return res.json();
       })
-      .then(json => {
+      .then((json) => {
         setUserData(json);
         setIsLoggedIn(true);
       })
@@ -51,7 +59,10 @@ export function App() {
         <Route path="/" element={<Home />} />
         <Route path="/search" element={<Search />} />
         <Route path="/map" element={<MapSearch />} />
-        <Route path="/landlord/:landlord_id" element={<Profile userData={userData} isLoggedIn={isLoggedIn}/>} />
+        <Route
+          path="/landlord/:landlord_id"
+          element={<Profile userData={userData} isLoggedIn={isLoggedIn} />}
+        />
         <Route
           path="/review/:landlord_id"
           element={<ReviewPage userData={userData} />}
@@ -72,4 +83,6 @@ export function App() {
       <Footer />
     </>
   );
-}
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
---
+++ client/src/actions/actions.js
@@ -0,0 +1,13 @@
+import * as types from './actionTypes';
+
+// updates state to hold current user data
+export const setUserData = user => ({
+  type: types.SET_USER,
+  payload: user,
+});
+
+// removes current user from state upon logout
+export const resetUserData = () => ({
+  type: types.RESET_USER,
+  payload: null,
+});
